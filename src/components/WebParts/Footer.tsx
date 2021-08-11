cbc7a728f4f51fa7e024c18929a34da9cffe9826
Miloš Havlíček <miloshavlicek@gmail.com>
2021-08-11 10:42:11 +0200

    Created simple home page, bootstrap upgraded from 4.6.x to 5.0.x

--- src/components/WebParts/Footer.tsx
+++ src/components/WebParts/Footer.tsx
@@ -1,31 +1,30 @@
 import { faGithub } from "@fortawesome/free-brands-svg-icons"
 import { useTranslation } from "next-i18next";
-import { IExternalLink } from "../../model/IExternalLink";
 import LinkExternal from "../uni/LinkExternal";
 
 export default function Footer() {
     const {t} = useTranslation();
 
-    const extLinks: IExternalLink[] = [
-        {
-            url: "https://github.com/miloshavlicek/dev4dev-website",
-            faIcon: faGithub,
-            title: "GitHub"
-        }
-    ]
-
     return (
-        <footer className="bd-footer p-3 p-md-5 mt-5 bg-white shadow-sm text-center text-sm-start">
+        <footer className="bd-footer p-3 p-md-5 bg-white shadow-sm text-center text-sm-start">
             <div className="container">
-                <ul className="bd-footer-links ps-0 mb-3">
-                    <li className="d-inline-block">
-                        {
-                            extLinks.map((extLink, i) =>
-                                <LinkExternal key={i} data={extLink} className="mx-3"/>
-                            )
-                        }
-                    </li>
-                </ul>
+                <div className="mb-2 text-center">
+                    {t("components.footer.developedBy")}
+                </div>
+
+                <div className="mt-2 mb-2 text-center">
+                    <LinkExternal
+                        url="https://github.com/miloshavlicek/dev4dev-website/issues"
+                        className="btn btn-secondary btn-lg me-1">
+                        {t("components.footer.reportIssue")}
+                    </LinkExternal>
+                    <LinkExternal
+                        url="https://github.com/miloshavlicek/dev4dev-website"
+                        faIcon={faGithub}
+                        className="btn btn-dark btn-lg me-1">
+                        {t("components.footer.editCode")}
+                    </LinkExternal>
+                </div>
             </div>
         </footer>
     )
